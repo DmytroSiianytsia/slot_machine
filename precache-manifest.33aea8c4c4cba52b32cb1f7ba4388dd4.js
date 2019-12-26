bc2a7fcbd73e3cc3f1f218c97dbe4163f07665de
Dima Siianytsia <ds081181@gmail.com>
2019-12-26 15:45:22 +0200

    Updates

---
+++ precache-manifest.33aea8c4c4cba52b32cb1f7ba4388dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c509c350dcb70c9b7b67c984748a3179",
+    "url": "/slot_machine/index.html"
+  },
+  {
+    "revision": "c6d8cbf2647c7369b0b5",
+    "url": "/slot_machine/static/css/main.e0c5009e.chunk.css"
+  },
+  {
+    "revision": "a449df4c481fa89173cb",
+    "url": "/slot_machine/static/js/2.2231573b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/slot_machine/static/js/2.2231573b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6d8cbf2647c7369b0b5",
+    "url": "/slot_machine/static/js/main.6d3a025e.chunk.js"
+  },
+  {
+    "revision": "27624f28414592e26dd6",
+    "url": "/slot_machine/static/js/runtime-main.fcde4df8.js"
+  },
+  {
+    "revision": "2796883de2ace009a4e2df074e4efdcd",
+    "url": "/slot_machine/static/media/sea.2796883d.jpg"
+  }
+]);
